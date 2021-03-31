2b801d85666a8c201570460477c93666c5f989d3
AdrianaRSilvagit config --global user.email adrianarafael2010@gmail.com <adrianarafael2010@gmail.com>
2021-03-31 13:19:51 -0300

    atualizar

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 
-const express = require('express')
-const app  =express()
+const express = require('express');
+const app  =express();
+
 //console.log(app) para verificar se está fucionando
-app.get ('/',(request , response)=> {
-    response.send('Olá, mundo!') 
+app.get ('/projects',(request, response)=> {
+   const{title, owner} =request.query
+   console.log(title)
+   console.log(owner)
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'projeto 100'
+    ])
+         
+    });
+    app.post('/projects',(request, response)=> {
+        return response.json([
+           'Projeto 1',
+           'Projeto 2',
+           'projeto 3',
+           'projeto 4'
+        ])
+    })
+    app.put('/projects/:id', (request, response)=> {
+        const params =request.params;
+        console.log(params)
+        return response.json([
+           'Projeto 1',
+           'Projeto 2',
+           'projeto 3',
+           'projeto 4',
+           'projeto 5'
+        ])
+    })
+    app.delete('/projects/:id', (request, response)=> {
+        return response.json([
+           'Projeto 50',
+           'Projeto 2',
+          
+        ])
     })
     
-    app.listen(3000, ()=>{               
-        console.log("servidor rodando!")
-    })
+    app.listen(3000);
         
